f0d19a0c8f0b9dc7407db1bc9e57022de89e3bde
dfsq <dfsq.dfsq@gmail.com>
2015-05-25 10:40:05 +0200

    Simple implementation added.

--- src/text-progress.js
+++ src/text-progress.js
@@ -25,9 +25,103 @@
 
 	'use strict';
 
-	function TextProgress() {
+	// Helper utility functions
+	var utils = {
 
+		mixin: function mix() {
+			var arg, prop, result = {};
+			for (arg = 0; arg < arguments.length; arg++) {
+				for (prop in arguments[arg]) {
+					if (arguments[arg].hasOwnProperty(prop)) {
+						result[prop] = arguments[arg][prop];
+					}
+				}
+			}
+			return result;
+		},
+
+		template: function(str, data) {
+			return str.replace(/\${(.*?)}/g, function(a, b) {
+				return typeof data[b] !== 'undefined' ? data[b] : '';
+			});
+		}
+	};
+
+
+	function TextProgress(options) {
+		this.options = utils.mixin({
+			max: 100
+		}, options);
+		this.element = this.options.element;
+		this.element.classList.add('progress');
+		this.progressType = this.getRenderer(options.type);
 	}
+
+	TextProgress.prototype = {
+
+		constructor: TextProgress,
+
+		display: function() {
+			this.element.innerHTML = this.progressType.render(this.getPercent(), this.value, this.options.max);
+		},
+
+		set: function(value) {
+			this.value = value;
+			this.display();
+		},
+
+		increment: function(value) {
+			this.set(this.value + value);
+		},
+
+		getRenderer: function(type) {
+			var options = typeof type === 'object' ? type : {name: type};
+			var renderer = new TextProgress.TYPES[options.name](options);
+			return renderer;
+		},
+
+		getPercent: function() {
+			return Math.ceil(this.value * 100 / this.options.max);
+		}
+	};
+
+	TextProgress.TYPES = {
+
+		simple: function() {
+			return {
+				render: function(percent) {
+					return percent + '%';
+				}
+			};
+		},
+
+		terminal: function(options) {
+
+			var defaults = {
+				width: 20,
+				charCompleted: '=',
+				charRemaining: '-',
+				template: '[<span class="completed">${completed}</span><span class="remaining">${remaining}</span>] ${percent}%'
+			};
+
+			return {
+				options: utils.mixin(defaults, options),
+				render: function(percent) {
+
+					var completed = Math.floor(this.options.width * percent / 100),
+						remaining = this.options.width - completed,
+						completedStr = new Array(completed + 1).join(this.options.charCompleted),
+						remainingStr = new Array(remaining + 1).join(this.options.charRemaining);
+
+					return utils.template(this.options.template, {
+						completed: completedStr,
+						remaining: remainingStr,
+						percent: percent
+					});
+				}
+			};
+		}
+	};
 
 	return TextProgress;
 }));
